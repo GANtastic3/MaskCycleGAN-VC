cacb3258af693fa3afcb549cdb6de90a775a9196
Hikaru Hotta <hhotta@amalfi.stanford.edu>
2021-03-26 15:37:52 -0700

    updated path in bash script

--- bash_scripts/mask_cyclegan_train.sh
+++ bash_scripts/mask_cyclegan_train.sh
@@ -4,7 +4,7 @@
 python -W ignore::UserWarning -m mask_cyclegan_vc.train \
     --name mask_cyclegan_vc_VCC2SF3_VCC2TF1 \
     --seed 0 \
-    --save_dir results/cycleGAN_VC3 \
+    --save_dir results/ \
     --preprocessed_data_dir vcc2018_preprocessed/vcc2018_training \
     --speaker_A_id VCC2SF3 \
     --speaker_B_id VCC2TF1 \
@@ -18,4 +18,3 @@ python -W ignore::UserWarning -m mask_cyclegan_vc.train \
     --num_frames 64 \
     --max_mask_len 25 \
     --gpu_ids 0 \
-    --continue_train \
