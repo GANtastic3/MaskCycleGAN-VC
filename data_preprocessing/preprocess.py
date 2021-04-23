3db4eb6fa0f125a897975f1e16974802399371ab
terbed <terbed@gmail.com>
2021-04-23 15:52:48 +0200

    No need for cliping ends

--- data_preprocessing/preprocess.py
+++ data_preprocessing/preprocess.py
@@ -31,7 +31,7 @@ def normalize_mel(wavspath):
     mel_list = list()
     for wavpath in tqdm(wav_files, desc='Preprocess wav to mel'):
         wav_orig, _ = librosa.load(wavpath, sr=SAMPLING_RATE, mono=True)
-        wav_orig, _ = librosa.effects.trim(wav_orig, top_db=15)  # clip silent both sides
+        # wav_orig, _ = librosa.effects.trim(wav_orig, top_db=15)  # clip silent both sides
         spec = vocoder(torch.tensor([wav_orig]))
         if spec.shape[-1] >= 64:
             mel_list.append(spec.cpu().detach().numpy()[0])
